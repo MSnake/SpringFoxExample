ab9325ce2e884afd8f8ec751eb84e69e99883bf0
shigaev_ak <shigaev_ak@ursip.ru>
2017-06-19 11:23:05 +0300

    - Car model mapping for SpringFox

--- src/main/java/com/msnake/alex/model/Car.java
+++ src/main/java/com/msnake/alex/model/Car.java
@@ -1,6 +1,8 @@
 package com.msnake.alex.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.*;
 
 import javax.persistence.*;
@@ -22,23 +24,27 @@ import java.io.Serializable;
 @JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 @Entity
 @Table(name = "car")
+@ApiModel(value = "Car")
 public class Car implements Serializable{
 
     /**
-     * Order id
+     * Car id
      */
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @ApiModelProperty(value = "Car Id", dataType = "Integer")
     private Integer id;
 
     /**
-     * Order name
+     * Car name
      */
+    @ApiModelProperty(value = "Car name", dataType = "String")
     private String name;
 
     /**
-     * Order full name
+     * Car full name
      */
+    @ApiModelProperty(value = "Car full name", dataType = "String")
     private String fullName;
 
 }
